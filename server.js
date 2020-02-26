98cad094b168de302f82ad5c5b8b3b7d987f2153
Hy-M <humayraa.m@outlook.com>
2020-02-26 09:22:15 +0000

    add cors

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const apiRouter = require("./routes/apiRouter");
 const { handle404, handle400, handle500 } = require("./errors/errors");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
